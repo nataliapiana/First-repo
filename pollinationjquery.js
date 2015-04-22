ade85afe5f4e89f93a8173b9c26a114003d0fec6
natalia piana <missnataliapiana@gmail.com>
2015-04-22 18:57:51 +0200

    add pollinationjquery.js

--- pollinationjquery.js
+++ pollinationjquery.js
@@ -4,39 +4,51 @@ $( document ).ready(function() {
 
 	     alert( "It works!!" );
 	 });
-    
-    $("#three").delay(5000).fadeIn( "slow", function() {
-    $("#four").fadeIn(100, "slow");
-});	
-	$("#line_one")delay(5000).fadeIn(0, function(){
-		$("#line_one").animate({
-			height: 70px
-			)};
-	}):	
-    			
-    $("#clickfront").click(function(){
-        $("#frontp").hide();
-     });
+});
 
-    $("#h").click(function(){
-     	$("#frontp").show();
-	});
-    
-    $("#pedicel").click(function(){
-        $("#pedicel").hide();
-    });
-    
-    $("#pedicel").click(function(){
-     	$("#pedicel").show();
-	});
-    
-    $("#rightp").click(function(){
-        $("#rightp").hide();
-    });
-     
-    $("#rightp").click(function(){
-     	$("#rightp").show();
-	});
+
+$( ".petals" ).hover(function() {
+        $( "#first_petal_path" ).fadeIn(100, "slow");
  });
+
+$( "#frontpetal, #rightpetal, #leftpetal, #backrightpetal, #backleftpetal" ).click(function() {
+  $( this ).hide( 1000, function() {
+    $( this ).remove();
+  });
+});
+    
+//     $("#three").delay(5000).fadeIn( "slow", function() {
+//     $("#four").fadeIn(100, "slow");
+// });	
+// 	$("#line_one")delay(5000).fadeIn(0, function(){
+// 		$("#line_one").animate({
+// 			height: 70px;
+// 			)};
+// 	}):	
+    			
+//     $("#clickfront").click(function(){
+//         $("#frontp").hide();
+//      });
+
+//     $("#h").click(function(){
+//      	$("#frontp").show();
+// 	});
+    
+//     $("#pedicel").click(function(){
+//         $("#pedicel").hide();
+//     });
+    
+//     $("#pedicel").click(function(){
+//      	$("#pedicel").show();
+// 	});
+    
+//     $("#rightp").click(function(){
+//         $("#rightp").hide();
+//     });
+     
+//     $("#rightp").click(function(){
+//      	$("#rightp").show();
+// 	});
+//  });
  
 
